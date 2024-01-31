b1894d546d7dc23eb3d16df39a67ee84a01babfb
AlexandreBessard <alexandre.bessard@gmail.com>
2024-01-31 15:55:52 +0100

    about section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {HeaderComponent} from "./header/header.component";
 import {HeroComponent} from "./hero/hero.component";
+import {MainComponent} from "./main/main.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, HeroComponent],
+  imports: [RouterOutlet, HeaderComponent, HeroComponent, MainComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
