999c0a0bbded31d12ec42ef5b2a1f643d4555e75
AlexandreBessard <alexandre.bessard@gmail.com>
2024-02-22 14:27:38 +0100

    client's image

---
+++ src/app/main/portfolio/portfolio-details/data-portfolio-details.ts
@@ -0,0 +1,8 @@
+export const portfolioDetails = [
+  {
+    id: '1'
+  },
+  {
+    id: '2'
+  }
+];
--- src/app/main/portfolio/portfolio-details/portfolio-details.component.ts
+++ src/app/main/portfolio/portfolio-details/portfolio-details.component.ts
@@ -1,24 +1,38 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
 import {ActivatedRoute, Router} from "@angular/router";
-import {CommonModule} from "@angular/common";
+import {AsyncPipe, CommonModule} from "@angular/common";
+import {PortfolioDetailsService} from "./portfolio-details.service";
+import {filter, map, Observable, tap} from "rxjs";
 
 @Component({
   selector: 'app-portfolio-details',
   standalone: true,
-  imports: [],
+  imports: [
+    AsyncPipe,
+    CommonModule
+  ],
   templateUrl: './portfolio-details.component.html',
   styleUrl: './portfolio-details.component.css'
 })
 export class PortfolioDetailsComponent implements OnInit {
 
-  id: string | null | undefined;
+  portfolioService = inject(PortfolioDetailsService);
+  portfolio$!: Observable<any>;
+  id?: string | null;
 
   constructor(private route: ActivatedRoute, private router: Router) {}
 
   ngOnInit(): void {
-    this.id = this.route.snapshot.paramMap.get('id');
-    if (this.id !== '1' && this.id !== '2') {
-      this.router.navigate(['/']); // Assuming home page route is '/'
-    }
+    this.portfolio$ = this.route.paramMap.pipe(
+      map((params) => params.get('id')),
+      filter((id) => !!id),
+      map((id) => {
+        if ((id !== '1' && id !== '2')) {
+          this.router.navigate(['/']);
+          return null;
+        }
+        return this.portfolioService.getById(id as string) || null;
+      })
+    );
   }
 }
