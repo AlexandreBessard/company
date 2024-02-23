af1aebfd91f7b8fcfd093477f6b1328dc9462400
AlexandreBessard <alexandre.bessard@gmail.com>
2024-02-23 12:24:28 +0100

    project details

--- src/app/main/portfolio/portfolio-details/data-portfolio-details.ts
+++ src/app/main/portfolio/portfolio-details/data-portfolio-details.ts
@@ -9,7 +9,7 @@ export const portfolioDetails = [
     description: 'Les utilisateurs peuvent consulter une liste d\'employés et effectuer des opérations CRUD (Create, Read, Update, Delete).\n' +
       ' Pour plus de reseignements, cliquer sur le lien ci-dessous:',
     categorie: 'Interface Web',
-    linkInformation: 'https://alexandrebessard.github.io/resume/project/details'
+    linkInformation: 'https://alexandrebessard.github.io/resume'
   },
   {
     id: '2',
@@ -18,10 +18,9 @@ export const portfolioDetails = [
     github: 'https://github.com/AlexandreBessard/fullstack-app-using-java-angular',
     photo: 'assets/img/portfolio/projet2.png',
     descriptionTitle: 'descriptionTitle',
-    description: "\n" +
-      "Application web Full Stack développée avec Angular et Spring Boot ! Une fois qu'un utilisateur existant est connecté avec succès, vous pouvez effectuer des opérations CRUD. L'état est persisté dans une base de données PostgreSQL." +
-      " Utiliser le mode incognito de Google Chrome en utilisant les identifiants suivant: Username: admin Password: admin",
+    description: 'Application web Full Stack développée avec Angular et Spring Boot ! Une fois qu\'un utilisateur existant est connecté avec succès, vous pouvez effectuer des opérations CRUD. Les données sont sauvegardées dans une base de données PostgreSQL. Pour tester l\'application en mode incognito sur Google Chrome, utilisez les identifiants suivants : Nom d\'utilisateur : admin Mot de passe : admin.' +
+      ' Il y a aussi la possibilite de creer un nouveau compte qui sera persiste en base de donnees.',
     categorie: 'Application web Fullstack avec base de donnees',
-    linkInformation: 'https://alexandrebessard.github.io/resume/project/details'
+    linkInformation: 'https://alexandrebessard.github.io/resume'
   }
 ];
