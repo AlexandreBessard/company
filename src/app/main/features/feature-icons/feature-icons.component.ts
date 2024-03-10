e93638fe55ce77d97bd662bb030dace82830b75f
AlexandreBessard <alexandre.bessard@gmail.com>
2024-03-10 12:32:32 +0100

    translation in progress

--- src/app/main/features/feature-icons/feature-icons.component.ts
+++ src/app/main/features/feature-icons/feature-icons.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {TranslateModule} from "@ngx-translate/core";
 
 @Component({
   selector: 'app-feature-icons',
   standalone: true,
-  imports: [],
+  imports: [
+    TranslateModule
+  ],
   templateUrl: './feature-icons.component.html',
   styleUrl: './feature-icons.component.css'
 })
--- src/app/main/features/features.component.ts
+++ src/app/main/features/features.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import {FeatureIconsComponent} from "./feature-icons/feature-icons.component";
+import {TranslateModule} from "@ngx-translate/core";
 
 @Component({
   selector: 'app-features',
   standalone: true,
   imports: [
-    FeatureIconsComponent
+    FeatureIconsComponent,
+    TranslateModule
   ],
   templateUrl: './features.component.html',
   styleUrl: './features.component.css'
