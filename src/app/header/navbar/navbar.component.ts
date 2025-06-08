e9879b1caa4eb816328bdeb0d32b433c21b68b75
alex <alexandre.bessard@gmail.com>
2025-06-08 11:46:14 +0200

    navbar improved

--- src/app/header/navbar/navbar.component.ts
+++ src/app/header/navbar/navbar.component.ts
@@ -1,7 +1,7 @@
 import {Component, OnInit, Renderer2} from '@angular/core';
 import {Router, RouterModule} from "@angular/router";
 import {NgStyle} from "@angular/common";
-import {TranslateModule} from "@ngx-translate/core";
+import {TranslateModule, TranslateService} from "@ngx-translate/core";
 declare var bootstrap: any;
 
 
@@ -14,12 +14,18 @@ declare var bootstrap: any;
 })
 export class NavbarComponent implements OnInit{
 
+  private toggle: boolean = false;
+  defaultLanguageIsEnglish: boolean = true;
 
-  constructor(private router: Router) {}
+
+  constructor(private router: Router,
+              private translate: TranslateService) {}
 
   ngOnInit(): void {
+    this.setDefaultLanguage();
+
     // Initialize Bootstrap components
-    var navbarCollapse = new bootstrap.Collapse(document.getElementById('navbarNav'));
+    //var navbarCollapse = new bootstrap.Collapse(document.getElementById('navbarNav'));
   }
 
   scrollToElement(elementId: string) {
@@ -31,6 +37,34 @@ export class NavbarComponent implements OnInit{
         }
       }, 100); // Adjust the timeout value as needed to ensure the home page is fully loaded before scrolling
     });
+  }
+
+  toggleLanguage(): void {
+    this.toggle = !this.toggle;
+    const newLanguage = this.toggle ? 'fr' : 'en';
+    this.translate.use(newLanguage);
+    // Store language preference in local storage
+    localStorage.setItem('preferredLanguage', newLanguage);
+  }
+
+  private setDefaultLanguage(): void {
+    const language = this.translate.getBrowserLang() || 'en';
+    if (localStorage.getItem("preferredLanguage") === 'fr') {
+      const language = localStorage.getItem("preferredLanguage");
+      if (language === 'fr') {
+        this.defaultLanguageIsEnglish = false;
+        this.toggle = true;
+        localStorage.setItem("preferredLanguage", 'fr');
+        return;
+      } else {
+        this.defaultLanguageIsEnglish = true;
+        this.toggle = false;
+        localStorage.setItem("preferredLanguage", 'en');
+      }
+    } else if (language === 'en') {
+      this.defaultLanguageIsEnglish = true;
+      localStorage.setItem("preferredLanguage", language);
+    }
   }
 
 
