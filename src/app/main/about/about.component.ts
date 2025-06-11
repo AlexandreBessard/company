ecc846d002e33dbe3c556a923bad1392fbd98996
alex <alexandre.bessard@gmail.com>
2025-06-11 15:15:44 +0200

    Improvements

--- src/app/main/about/about.component.ts
+++ src/app/main/about/about.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { NgOptimizedImage } from '@angular/common'
 import {TranslateModule} from "@ngx-translate/core";
+import {SanitizeHtmlPipe} from "../../pipes/sanitizeHtml.pipe";
 
 @Component({
   selector: 'app-about',
   standalone: true,
   imports: [
     NgOptimizedImage,
-    TranslateModule
+    TranslateModule,
+    SanitizeHtmlPipe
   ],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
--- src/app/main/faq/faq.component.ts
+++ src/app/main/faq/faq.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {TranslateModule} from "@ngx-translate/core";
+import {SanitizeHtmlPipe} from "../../pipes/sanitizeHtml.pipe";
 
 @Component({
   selector: 'app-faq',
   standalone: true,
-  imports: [],
+  imports: [
+    TranslateModule,
+    SanitizeHtmlPipe
+  ],
   templateUrl: './faq.component.html',
   styleUrl: './faq.component.css'
 })
 export class FaqComponent {
-
 }
