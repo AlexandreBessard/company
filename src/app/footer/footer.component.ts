58072c769f8035992be8f02d4dad722e895c877a
alex <alexandre.bessard@gmail.com>
2025-06-11 22:00:18 +0200

    Improvements

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import {TranslateModule} from "@ngx-translate/core";
+import {SanitizeHtmlPipe} from "../pipes/sanitizeHtml.pipe";
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [
+    TranslateModule,
+    SanitizeHtmlPipe
+  ],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
